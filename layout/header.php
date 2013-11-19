1e26cfa5f87b7990413b04d501ff2933cd7358bf
Morgan BOURSE <morgan.bourse@gmail.com>
2013-11-20 00:59:36 +0100

    ameliorations graphiques

--- layout/header.php
+++ layout/header.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>MicroGames62</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="keywords" content="MicroGames62, ordinateur, computer, console, ps, playstation, xbox, telephone, deblocage" />
 <meta name="description" content="MicroGames62 - Réparation de consoles de jeux, informatique, telephonie" />
+<title>MicroGames62</title>
+
 <link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
 <link href="css/cupertino/jquery-ui-1.10.3.custom.css" rel="stylesheet" type="text/css" />
 
--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,35 +1,34 @@
 <div id="templatemo_sidebar">
+	<h2>News</h2>
 	<div id="news_box">
-		<h2>Latest Updates</h2>
-				
-		<ul>
-			<li>
-				<h6><a href="#">Lorem ipsum dolor sit amet</a></h6>
-				<p>You may validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow">CSS</a> for this page. Maecenas tellus erat, dictum vel semper, dapibus ac elit.</p>
-			</li>
-			
-			<li>
-				<h6><a href="#">Nunc porttitor feugiat elit</a></h6>
-				<p>Nunc porttitor feugiat elit, ac tristique turpis condimentum in. Sed eleifend odio eget massa venenatis elementum.</p>
-			</li>
-			
-			<li class="last">
-				<h6><a href="#">Pellentesque mauris ligula</a></h6>
-				<p>Integer tempus consectetur risus, id congue justo fringilla vel. Curabitur vestibulum vulputate consectetur. </p>
-			</li>
-			
-		</ul>
-		<div class="btn_more"><a href="#"><span>+</span> View All Posts</a></div>
+<!--		<ul>-->
+<!--			<li>-->
+<!--				<h6><a href="#">Lorem ipsum dolor sit amet</a></h6>-->
+<!--				<p>You may validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow">CSS</a> for this page. Maecenas tellus erat, dictum vel semper, dapibus ac elit.</p>-->
+<!--			</li>-->
+<!--			-->
+<!--			<li>-->
+<!--				<h6><a href="#">Nunc porttitor feugiat elit</a></h6>-->
+<!--				<p>Nunc porttitor feugiat elit, ac tristique turpis condimentum in. Sed eleifend odio eget massa venenatis elementum.</p>-->
+<!--			</li>-->
+<!--			-->
+<!--			<li class="last">-->
+<!--				<h6><a href="#">Pellentesque mauris ligula</a></h6>-->
+<!--				<p>Integer tempus consectetur risus, id congue justo fringilla vel. Curabitur vestibulum vulputate consectetur. </p>-->
+<!--			</li>-->
+<!--			-->
+<!--		</ul>-->
+<!--		<div class="btn_more"><a href="#"><span>+</span> View All Posts</a></div>-->
 		<div class="cleaner"></div>     
 	</div>
 	
-	<div class="sidebar_box">
-		<h2><a href="http://fr.photohq.com" title="photos gratuites"  class="sidebar_box_h2" target="_blank">Testimonial</a></h2>
-		<blockquote>
-			<p align="justify">Quisque sit amet adipiscing libero. Donec et tellus vitae velit pharetra lobortis. Ut commodo, nulla eu pellentesque mollis, odio ante condimentum augue, eget facilisis dui magna ut ligula.</p>
-			
-			<cite>David - <span><a href="portfolio.html">Marketing Specialist</a></span></cite>
-		</blockquote>
-	</div>
+<!--	<div class="sidebar_box">-->
+<!--		<h2><a href="http://fr.photohq.com" title="photos gratuites"  class="sidebar_box_h2" target="_blank">Testimonial</a></h2>-->
+<!--		<blockquote>-->
+<!--			<p align="justify">Quisque sit amet adipiscing libero. Donec et tellus vitae velit pharetra lobortis. Ut commodo, nulla eu pellentesque mollis, odio ante condimentum augue, eget facilisis dui magna ut ligula.</p>-->
+<!--			-->
+<!--			<cite>David - <span><a href="portfolio.html">Marketing Specialist</a></span></cite>-->
+<!--		</blockquote>-->
+<!--	</div>-->
 	
 </div>
